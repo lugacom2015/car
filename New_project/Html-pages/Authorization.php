ab273f338753ee36bb81e2759308ccdb0c1b6d5c
Eugene-Balabat <djek1099@mail.ru>
2017-07-11 17:21:25 +0400

    first commit

---
+++ New_project/Html-pages/Authorization.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Авторизация</title>
+	<meta http-equiv="Content-Type" content="text/html" charset = "UTF-8">
+	<link rel = "stylesheet" type = "text/css" href = "../Css-files/Authorization.css"	 media = "all">	
+</head>
+<body>
+	<div class = "mid">
+		<form method = "POST">
+			<div class="bc-input">
+			<input type="text" name = "username" placeholder = "Введите логин">	
+			</div>
+			<div class = "bc-input">
+			<input type="password" name = "password" placeholder = "Введите пароль">	
+			</div>
+			<div class = "bc-submit">
+			<button type="submit" name = "submit">Войти</button>
+			</div>
+		</form>	
+	</div>
+</body>
+</html>
