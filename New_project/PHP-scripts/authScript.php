48cc2a51675b9033aa02f203c4dcebc9cb72fc50
ArtemLitvinov <tripolets1@gmail.com>
2017-07-17 20:04:00 +0300

    fixed authScript.php again

--- New_project/PHP-scripts/authScript.php
+++ New_project/PHP-scripts/authScript.php
@@ -4,18 +4,16 @@
         //запрос на выборку из БД, имена таблиц и полей не конечные и могут изменяться
         ->query("SELECT id,password,admin FROM user WHERE login='".$_POST["username"]."'");
         $userInfo=$authQuery->fetch_row();//переменная содержащая данные загруженые с помощью запроса
-	if ($userInfo[1]==$_POST["password"])//проверка на ввод правильного сочетания логин/пароль
+	if ($userInfo[1]==$_POST["password"] and $_POST["password"]!="")//проверка на ввод правильного сочетания логин/пароль
     {
         if ($userInfo[2]==FALSE)//проверка является ли админом пользователь
         {
-            header("Location: ../html-pages/Application.php?id=".$userInfo[0]);
-            //echo "Введено правильное сочетание логин/пароль";
+            header("Location: ../html-pages/request.php?id=".$userInfo[0]);
             exit;
         }
         else
         {
-            //header("Location: ../html-pages/admin.php");
-            echo "Введено правильное сочетание логин/пароль";
+            header("Location: ../html-pages/admin.php");
             exit;
         }
     }
