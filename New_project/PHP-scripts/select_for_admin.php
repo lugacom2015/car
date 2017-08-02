b57a758b7df5bcb5ec8ee7233bd7ba00abdb4d70
Eugene-Balabat <djek1099@mail.ru>
2017-08-02 23:00:40 +0400

    fixed select_for_admin

--- New_project/PHP-scripts/select_for_admin.php
+++ New_project/PHP-scripts/select_for_admin.php
@@ -36,9 +36,9 @@
                         echo "<td>";
                         echo (substr($timeTo, 0, -3));
                         echo "</td>";
-						echo "<form action = '#' name='str' method='post' onsubmit = 'return false;'>";
+						echo "<form action = '../php-scripts/requestEnd.php?id=".$idRequest."' name='str' method='post'>";
                         echo "<td>";
-                        echo "<select id = 'car".$idRequest."' name = 'car' class='car'>";
+                        echo "<select name = 'car' class='car'>";
                                 echo "<option disabled selected = 'selected'>Выберите машину</option>";     
                                 $queryCar = mysqli_query($dbConnection, "SELECT * FROM car");                                    
                                 while ($resultCar = mysqli_fetch_assoc($queryCar)) 
@@ -48,7 +48,7 @@
                         echo "</select>";
                         echo "</td>";
                         echo "<td>";
-                        echo "<select id = 'driver".$idRequest."' name = 'driver' class='driver'>";
+                        echo "<select name = 'driver' class='driver'>";
                                 echo "<option disabled selected = 'selected'>Выберите водителя</option>";     
                                 $queryDriver= mysqli_query($dbConnection, "SELECT * FROM driver");                                    
                                 while ($resultDriver = mysqli_fetch_assoc($queryDriver)) 
@@ -58,11 +58,11 @@
                         echo "</select>";
                         echo "</td>";
                         echo "<td class = 'memo'>";
-                        echo "<textarea id = 'desc".$idRequest."' name='desc'>";
+                        echo "<textarea name='desc'>";
                         echo "</textarea>";
                         echo "</td>";
                         echo "<td class = 'savetd'>";
-                               echo "<button id = '".$idRequest."' data-id = '".$idRequest."' type = 'submit' name = 'save' class = 'saved'>Сохранить</button>";
+                                echo "<button id = '".$idRequest."' data-id = '".$idRequest."' type = 'submit' name = 'save' class = 'saved'>Сохранить</button>";
                         echo "</td>";
                 echo "</tr>";     
 				echo "</form>";
