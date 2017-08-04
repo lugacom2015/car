01130a47965045862b2b1eccc6c7ab654ec15214
ruskait <work12334@mail.ru>
2017-08-04 20:15:42 +0400

    Add files via upload

--- New_project/Html-pages/Admin.php
+++ New_project/Html-pages/Admin.php
@@ -1,193 +1,188 @@
-<?php
-	session_start();
-	if(!isset($_SESSION['username']) || !isset($_SESSION['password']))
+<? PHP
+session_start ();
+	If ( ! Isset ( $ _SESSION [ ' username ' ]) ||  ! Isset ( $ _SESSION [ ' password ' ]))
 	{
-		header('Location: Authorization.php');
+		Header ( ' Location: Authorization.php ' );
 	}
-	include_once('../PHP-scripts/connectScript.php');
-?>
-<html>
-<head>
-	<title>Страница администратора</title>
-	<meta http-equiv="Content-Type" content="text/html" charset = "UTF-8">
-	<link rel = "stylesheet" type = "text/css" href = "../Css-files/Admin.css" media = "all">	
-	<link rel = "stylesheet" type = "text/css" href = "../Css-files/table.css" media = "all">	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  	<script type = "text/javascript" src = "../Java_Script/JQuery/jquery.timepicker.js"></script>
-  	<link rel = "stylesheet" type="text/css" href = "../Css-files/jquery.timepicker.css" />
-  	<script type = "text/javascript" src = "../Java_Script/JQuery/bootstrap-datepicker.js"></script>
-  	<link rel="stylesheet" type="text/css" href="../Css-files/bootstrap-datepicker.css" />
-  	<script type="text/javascript">	
-		function funckSuccess(idRequest,carId,driverId) 
+	include_once ( ' ../PHP-scripts/connectScript.php ' );
+? >
+< Html >
+< Head >
+	< Название > Страница администратора </ название >
+	< Meta  http-equiv = " Content-Type "  content = " text / html "  charset = " UTF-8 " >
+	< Link  rel = " stylesheet "  type = " text / css "  href = " ../Css-files/Admin.css "  media = " all " >	
+	< Link  rel = " stylesheet "  type = " text / css "  href = " ../Css-files/table.css "  media = " all " >	
+	< Script  src = " https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js " > < / script >
+  	< Script  type = " text / javascript "  src = " ../Java_Script / JQuery / jquery.timepicker.js " > < / script >
+  	< Link  rel = " stylesheet "  type = " text / css "  href = " ../Css-files/jquery.timepicker.css " />
+  	< Script  type = " text / javascript "  src = " ../Java_Script / JQuery / bootstrap-datepicker.js " > < / script >
+  	< Link  rel = " stylesheet "  type = " text / css "  href = " ../Css-files/bootstrap-datepicker.css " />
+  	< Script  type = " text / javascript " >	
+		Функция  funckSuccess ( idRequest , carId , driverId )
 		{
-			$("#"+idRequest).css ("background", "#32CD32");
-			$("#"+idRequest).text ("Сохранено");
-			$("#"+idRequest).mouseover(function(event)
+			$ ( " # " + IdRequest). Css ( « фон » , « # 32CD32 » );
+			$ ( " # " + IdRequest). Текст ( « Сохранено » );
+			$ ( " # " + IdRequest). Mouseover ( функция ( событие )
 			{
-  				$(this).css("border", "2px solid #32CD32");
-  				$(this).css("transition", "all 0.5s");
-  				$(this).css("background", "#fff");
-  				$(this).css("color", "#32CD32");
+  				$ ( Это ). Css ( " border " , " 2px solid # 32CD32 " );
+  				$ ( Это ). Css ( « переход » , « все 0,5 с » );
+  				$ ( Это ). Css ( « фон » , « #fff » );
+  				$ ( Это ). Css ( « цвет » , « # 32CD32 » );
   			});
-  			$("#"+idRequest).mouseout(function(event) 
+  			$ ( " # " + IdRequest). Mouseout ( функция ( событие )
 			{
-				$(this).css("background", "#32CD32");
-				$(this).css("transition", "all 1s");
-				$(this).css("border", "2px solid #fff");
-				$(this).css("color", "#fff");
+				$ ( Это ). Css ( « фон » , « # 32CD32 » );
+				$ ( Это ). Css ( « переход » , « все 1s » );
+				$ ( Это ). Css ( " border " , " 2px solid #fff " );
+				$ ( Это ). Css ( « цвет » , « #fff » );
 			});
-
-			$("#driver"+idRequest).change(function()
+			$ ( " #driver " + idRequest). Change ( function ()
 			{
-				$("#"+idRequest).css ("background", "#617a76");
-				$("#"+idRequest).text ("Сохранить");
-				$("#"+idRequest).mouseover(function(event)
+				$ ( " # " + IdRequest). Css ( « фон » , « # 617a76 » );
+				$ ( " # " + IdRequest). Текст ( « Сохранить » );
+				$ ( " # " + IdRequest). Mouseover ( функция ( событие )
 				{
-	  				$(this).css("border", "2px solid #617a76");
-	  				$(this).css("transition", "all 0.5s");
-	  				$(this).css("background", "#fff");
-	  				$(this).css("color", "#617a76");
+	  				$ ( Это ). Css ( « граница » , « 2px solid # 617a76 » );
+	  				$ ( Это ). Css ( « переход » , « все 0,5 с » );
+	  				$ ( Это ). Css ( « фон » , « #fff » );
+	  				$ ( Это ). Css ( « цвет » , « # 617a76 » );
 	  			});
-	  			$("#"+idRequest).mouseout(function(event) 
+	  			$ ( " # " + IdRequest). Mouseout ( функция ( событие )
 				{
-					$(this).css("background", "#617a76");
-					$(this).css("transition", "all 1s");
-					$(this).css("border", "2px solid #fff");
-					$(this).css("color", "#fff");
+					$ ( Это ). Css ( « фон » , « # 617a76 » );
+					$ ( Это ). Css ( « переход » , « все 1s » );
+					$ ( Это ). Css ( " border " , " 2px solid #fff " );
+					$ ( Это ). Css ( « цвет » , « #fff » );
 				});
 			});
-
-			$("#car"+idRequest).change(function()
+			$ ( " #car " + idRequest). Change ( function ()
 			{
-				$("#"+idRequest).css ("background", "#617a76");
-				$("#"+idRequest).text ("Сохранить");
-				$("#"+idRequest).mouseover(function(event)
+				$ ( " # " + IdRequest). Css ( « фон » , « # 617a76 » );
+				$ ( " # " + IdRequest). Текст ( « Сохранить » );
+				$ ( " # " + IdRequest). Mouseover ( функция ( событие )
 				{
-	  				$(this).css("border", "2px solid #617a76");
-	  				$(this).css("transition", "all 0.5s");
-	  				$(this).css("background", "#fff");
-	  				$(this).css("color", "#617a76");
+	  				$ ( Это ). Css ( « граница » , « 2px solid # 617a76 » );
+	  				$ ( Это ). Css ( « переход » , « все 0,5 с » );
+	  				$ ( Это ). Css ( « фон » , « #fff » );
+	  				$ ( Это ). Css ( « цвет » , « # 617a76 » );
 	  			});
-	  			$("#"+idRequest).mouseout(function(event) 
+	  			$ ( " # " + IdRequest). Mouseout ( функция ( событие )
 				{
-					$(this).css("background", "#617a76");
-					$(this).css("transition", "all 1s");
-					$(this).css("border", "2px solid #fff");
-					$(this).css("color", "#fff");
+					$ ( Это ). Css ( « фон » , « # 617a76 » );
+					$ ( Это ). Css ( « переход » , « все 1s » );
+					$ ( Это ). Css ( " border " , " 2px solid #fff " );
+					$ ( Это ). Css ( « цвет » , « #fff » );
 				});	
 			});
-
-
-			var selectCar = document.getElementsByClassName('car');
-			for (var i = 0; i <= selectCar.length-1; i++)
+			вар selectCar =  документ . GetElementsByClassName ( ' автомобиль ' );
+			для ( вар я =  0 ; я <=  selectCar . Длина - 1 ; я ++ )
 			{
-				var valueOptionCar = selectCar[i].value;
-				if(valueOptionCar != carId)
+				вар valueOptionCar = selectCar [I]. Стоимость ;
+				If (valueOptionCar ! = CarId)
 				{
-					for (var j = 0; j <= selectCar[i].options.length-1; j++)
+					для ( вар J =  0 ; J <= selectCar [I]. Варианты . Длина - 1 ; J ++ )
 					{
-						if (selectCar[i].options[j].value == carId) 
+						If (selectCar [i]. Options [j]. Значение  == carId)
 						{
-							selectCar[i].remove(j);
-							break;
+							selectCar [I]. Удалить (j);
+							Перерыв ;
 						}
 					}
 				}
 			}
 			
-			var selectDriver = document.getElementsByClassName('driver');
-			for (i = 0; i <= selectDriver.length-1; i++)
+			вар selectDriver =  документ . GetElementsByClassName ( ' driver ' );
+			для (я =  0 ; я <=  selectDriver . Длина - 1 ; я ++ )
 			{
-				var valueOptionDriver = selectDriver[i].value;
-				if(valueOptionDriver != driverId)
+				вар valueOptionDriver = selectDriver [I]. Стоимость ;
+				If (valueOptionDriver ! = DriverId)
 				{
-					for (j = 0; j <= selectDriver[i].options.length-1; j++)
+					для (J =  0 ; J <= selectDriver [I]. Варианты . Длина - 1 ; J ++ )
 					{
-						if (selectDriver[i].options[j].value == driverId) 
+						If (selectDriver [i]. Options [j]. Value  == driverId)
 						{
-							selectDriver[i].remove(j);
-							break;
+							selectDriver [I]. Удалить (j);
+							Перерыв ;
 						}
 					}
 				}
 			}
   		}
-
-		$(document).ready(function() 
+		$ ( Документ ). Ready ( функция ()
 		{
-			$(".saved").bind("click",function()
+			$ ( " .saved " ). Bind ( " click " , function ()
 			{
-				var idRequest = $(this).data('id');
-				var carId = document.getElementById('car' + idRequest).value;
-				var driverId = document.getElementById('driver' + idRequest).value;
-				var descId = document.getElementById('desc' + idRequest).value;
-				if(carId != "Выберите машину" && driverId != "Выберите водителя")
+				вар idRequest =  $ ( это ). Данные ( ' id ' );
+				вар carId =  документ . GetElementById ( ' car '  + idRequest). Стоимость ;
+				вар driverId =  документ . GetElementById ( ' driver '  + idRequest). Стоимость ;
+				вар descId =  документ . GetElementById ( ' desc '  + idRequest). Стоимость ;
+				If (carId ! =  " Выбрать машину "  && driverId ! =  " Выбрать водителя " )
 				{	
-					$.ajax 
+					$ . Аякса 
 					({
-						url: "../PHP-scripts/requestEnd.php",
-						type: "POST",
-						data: ({
-			    		car: carId, 
-			    		driver: driverId,
-			    		desc: descId,
-			   			id: idRequest
+						Url :  " ../PHP-scripts/requestEnd.php " ,
+						Тип :  « POST » ,
+						Данные : ({
+			    		Автомобиль : carId,
+			    		Driver : driverId,
+			    		по убыванию : descId,
+			   			Id : idRequest
 							}),
-						dataType: "html",
-						success: funckSuccess(idRequest,carId,driverId)
+						DataType :  " html " ,
+						Успех :  funckSuccess (idRequest, carId, driverId)
 					});
 				}
 			});
 		});
-	</script>
-</head>
-<body>
-	<div class = "mid">
-		<form action = "<?php echo $_SERVER['PHP_SELF']; ?>" name="admin" method="post">
-		<div>
-			<p id="datepairExample">
-			    <input type="text" name="ds" class="date start" value="" />
-			    <input type="text" name="ts" class="time start" value="" /> 
-			    <button type = "submit" name = "search">Поиск</button>
-			    <input type="text" name="df" class="date end" value="" />
-			    <input type="text" name="tf" class="time end" value="" />
-			</p>
-		</div>
-		</form>
-		<table class='table'>
-					<tr>
-						<th>ФИО</th>
-						<th>Откуда</th>
-						<th>Куда</th>
-						<th>Дата/Время с</th>
-						<th>Дата/Время по</th>
-						<th>Машина</th>
-						<th>Водитель</th>
-						<th>Примечание</th>
-						<th>Сохранить</th>
-					</tr>
-					<?php
-		                 	$queryRequestFirst = mysqli_query($dbConnection, "SELECT * FROM request_first WHERE view = 0");
-		                     while ($resultRequestFirst = mysqli_fetch_array($queryRequestFirst)) 
+	< / Script >
+</ Head >
+< Body >
+	< Div  class = " mid " >
+		< Форма  действия = " <PHP? Эхо $ _SERVER [ ' PHP_SELF ' ]; ? > "    Имя = " админ "  метод = " пост " >
+		< Div >
+			< P  id = " datepairExample " >
+			    < Input  type = " text "  name = " ds "  class = " date start "  value = " " />
+			    < Input  type = " text "  name = " ts "  class = " time start "  value = " " />
+			    < Button  type = " submit "  name = " search " > Поиск </ button >
+			    < Input  type = " text "  name = " df "  class = " date end "  value = " " />
+			    < Input  type = " text "  name = " tf "  class = " time end "  value = " " />
+			</ P >
+		</ Div >
+		</ Form >
+		< Table  class = ' table ' >
+					< Tr >
+						< Th > ФИО </ th >
+						< Th > Откуда </ th >
+						< Th > Куда </ th >
+						< Th > Дата / Время с </ th >
+						< Th > Дата / Время по </ th >
+						< Th > Машина </ th >
+						< Th > Водитель </ th >
+						< Th > Примечание </ th >
+						< Th > Сохранить </ th >
+					</ Tr >
+					<? PHP
+		                 	$ QueryRequestFirst  =  mysqli_query ( $ dbConnection , " SELECT  *  FROM request_first WHERE view =  0 " );
+		                     в то время как ( $ resultRequestFirst  =  mysqli_fetch_array ( $ queryRequestFirst ))
 		                     {
-	           					require '../PHP-scripts/select_for_admin.php';  
+	           					требуется  ' ../PHP-scripts/select_for_admin.php ' ;  
 		                     }		           
-					?>
-		</table>
-		<!--<button type = "submit" name = "save" class="save">Сохранить</button>-->
-		<script>
-	    		$('#datepairExample .time').timepicker({
-	       		 	'showDuration': true,
-	      	     	'timeFormat': 'H:i',
-	      	     	'step': 15
+					? >
+		</ Table >
+		<! - <button type = "submit" name = "save" class = "save"> Сохранить </ button> ->
+		< Script >
+	    		$ ( ' #datepairExample .time ' ). Timepicker ({
+	       		 	' ShowDuration ' :  true ,
+	      	     	' TimeFormat ' :  ' H: i ' ,
+	      	     	« Шаг » :  15
 	    		});
-			    $('#datepairExample .date').datepicker({
-			        'format': 'yyyy-m-d',
-			        'autoclose': true
+			    $ ( ' #datepairExample .date ' ). Datepicker ({
+			        ' Format ' :  ' yyyy-md ' ,
+			        ' Autoclose ' :  true
 			    });			    
-	    		$('#datepairExample').datepair();
-			</script>
-		</form>	
-	</div>
-</body>
-</html>
+	    		$ ( ' #datepairExample ' ). Datepair ();
+			< / Script >
+		</ Form >	
+	</ Div >
+</ Body >
+</ Html >
